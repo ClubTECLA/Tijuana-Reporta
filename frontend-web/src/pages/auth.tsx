3b24f5487f39bb8814212daeaff08df8be564a51
Daniel Gonzalez <juan.reyes10@uabc.edu.mx>
2026-09-16 22:40:22 -0700

    feat: the navigation logic encapsulated in the 'useRouter' hook

--- frontend-web/src/pages/auth.tsx
+++ frontend-web/src/pages/auth.tsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { useRouter } from '../hooks/useRouter'
+
 export default function AuthPage() {
+    const [searchParams] = useSearchParams()
+    const [ tab , setTab ] = useState('login');
+    
+    useEffect(() => {
+        const tabParam = searchParams.get('tab');
+        if (tabParam === 'login' || tabParam === 'register') {
+            setTab(tabParam);
+        }
+    }, []);
+
+    
+
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-4">Sign In</h1>
-            <form className="flex flex-col items-center">
-                <input type="text" placeholder="Username" className="border border-gray-300 rounded-md p-2 mb-4 w-64" />
-                <input type="password" placeholder="Password" className="border border-gray-300 rounded-md p-2 mb-4 w-64" />
-                <button type="submit" className="bg-blue-500 text-white rounded-md p-2 w-64">Sign In</button>
-            </form>
+            
         </div>  
     )
 }
