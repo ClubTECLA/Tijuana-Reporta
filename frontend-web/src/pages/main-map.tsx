1ed0cd7f2a9c0e15a986bf323e25c6b0599835a1
DanielGR <jdanielgr2005@gmail.com>
2026-09-22 21:15:02 -0700

    feat: UserProfileWidget Created

--- frontend-web/src/pages/main-map.tsx
+++ frontend-web/src/pages/main-map.tsx
@@ -1,11 +1,22 @@
 import BellButton from "../components/BellButton";
 import MapContainer from "../components/MapContainer";
+import UserProfileWidget from "../components/UserProfileWidget";
 
 export default function MainMapPage(){
+    
     return(
-        <section className="relative">
+        <section className="">
             <MapContainer/>
-            <BellButton className="absolute top-5 right-30 border-2 border-black"/>
+            <div className="fixed top-2 w-full h-13 flex flex-row justify-end pr-10 border-3">
+                <div className="flex flex-3/4 border-2 w-100">
+
+                </div>
+                <div className="flex flex-row flex-1/4 items-end justify-end gap-2 border-2">
+                    <BellButton className=""/>
+                    <UserProfileWidget className=""/>    
+                </div>
+            </div>
+            
         </section>
     )
 }
